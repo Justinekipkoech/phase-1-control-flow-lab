b473e8620a321a6bd3f27b37662ba8e6a81a24b2
justinekipkoech <justinekipkoech98@gmail.com>
2022-12-06 13:05:05 +0300

    completed

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
+//code here
+function scuberGreetingForFeet(feet) {
+  if (feet <= 400) {
+    return "This one is on me!";
+  }
+  else if (feet > 2000 && feet < 2500) {
+    return "I will gladly take your thirty bucks.";
+  }
+  else {
+    return "No can do.";
+  }
+}
+function ternaryCheckCity(city) {
+  return city=="NYC" ? "Ok, sounds good." : "No go.";
+   console.log (pittsbourgh)
   // Write your code here!
 }
+function switchOnCharmFromTip(tip) {
+  let result;
+  switch(tip){
+         case 'generous':
+         result = "Thank you so much.";
+      break;
+   case 'not as generous':
+   result= "Thank you.";
+      break;
+    default:
+      result= "Bye.";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+   }
+    return result;
+   }
